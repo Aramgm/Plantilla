61bb93b26e30c8a98ab36a3fa9c9d0910ede0893
Aramgm <aramgm10@gmail.com>
2016-02-05 14:50:06 +0100

    Explicació del programa Ll3

--- Ll3._El_primer_programa/Ll3._El_primer_programa/Ll3._El_primer_programa/Ll3._El_primer_programa..ino
+++ Ll3._El_primer_programa/Ll3._El_primer_programa/Ll3._El_primer_programa/Ll3._El_primer_programa..ino
@@ -9,18 +9,24 @@
 
 //******  Variables  *****************************************************
 int ledPlacaPin = 10;
-
+//Aquí seleccionem el pin de la placa on anirá del led
 
 //******  Setup  ********************************************************* 
 void setup() {
 pinMode(ledPlacaPin, OUTPUT);
+//Determina el pin insertat a dalt com a sortida
 }
 
 
 //******  Loop  ********************************************************** 
 void loop() {
-digitalWrite(ledPlacaPin, HIGH);   
-delay(1000);             
-digitalWrite(ledPlacaPin, LOW);    
-delay(1000);              
+digitalWrite(ledPlacaPin, HIGH); 
+//Obre el led  
+delay(1000);
+//Espera un segon amb el led encés             
+digitalWrite(ledPlacaPin, LOW);
+//Apaga el led    
+delay(1000);
+//Espera un segon amb el led apagat
+//Aquesta funció(Voild loop) es va repetint contínuament              
 }
