a56b77b939d6142585858fc5d66001b76c1e47c7
Aramgm <aramgm10@gmail.com>
2016-03-05 12:26:32 +0100

    Exercicis de Serial
    Exercici 5

---
+++ Serial_5/Serial_5.ino
@@ -0,0 +1,22 @@
+/*************************************************************************
+**    Intrueix els nobres assignats a cada lletra, calcula i mosatra    **
+**                 els resultats. Només ho fa un cop.                   **
+**                                                                      **  
+*************************************************************************/                                                                     
+
+//******  Includes  ******************************************************
+int a =5;   //Variables internes, d'assignació. S'assigna a cada lletra un
+int b =10;  //nombre que s'introduiex de forma automàtica cada cop que hi 
+int c =20;  //surt.
+//******  Setup  ********************************************************* 
+void setup() {        
+
+Serial.println(pow (6));
+
+}
+
+
+//******  Loop  ********************************************************** 
+void loop() {
+                        //No fa res        
+}
