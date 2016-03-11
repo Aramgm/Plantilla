bc58b4a3c44000fb024d376077eec5a54a354ae1
Aramgm <aramgm10@gmail.com>
2016-03-11 02:14:27 +0100

    Exercicis de Serial
    Exercici_19_Acabat

--- Serial_19/Serial_19.ino
+++ Serial_19/Serial_19.ino
@@ -11,9 +11,9 @@ float rSerie, rParalel;// Variables amb coma flotant per el resultat.
 void setup() { 
 Serial.begin(9600);        //Velocitat de càrrega d'escaneig
 
-Serial.println("Entra el valor de r1 i r2 (separats per una coma)");
+Serial.println("Entra el valor de R1 i R2 (separats per una coma)");
 //Escriu el text i salta de línea.
-Serial.println ();         //Deixa un espai en blanc.
+Serial.println();         //Deixa un espai en blanc.
 }
 
 //******  Loop  ********************************************************** 
@@ -23,42 +23,55 @@ while (Serial.available() > 0) {  // Si la variable es més gran que zero
                                   //càrrega el codi.
     r1 = Serial.parseInt();//Llegeix el primer nómbre sencer de la posició
                            //en la que es troba i el guarda a r1.
-    Serial.print ("R1=");  //Escriu R1=.
-    Serial.println ( r1) ; //Inserta el nombre de la variable r1.
+    Serial.print("R1=");   //Escriu R1=.
+    Serial.println( r1) ;  //Inserta el nombre de la variable r1.
     
     r2 = Serial.parseInt();//Llegeix el primer nómbre sencer de la posició
                            //en la que es troba i el guarda a r2.
 
-    Serial.print ("R2=");  //Escriu R2=.
-    Serial.println ( r2) ; //Inserta el nombre de la variable r2.
-    Serial.println ();     //Deixa un espai en blanc.
+    Serial.print("R2=");   //Escriu R2=.
+    Serial.println( r2) ;  //Inserta el nombre de la variable r2.
+    Serial.println();      //Deixa un espai en blanc.
    
     if (Serial.read() == '\n') { //Comproba el final de línea.
+      
+    delay(1000);           //Espera 1 segon.
     
-    Serial.println ("Resistencies en serie:");//Escriu el text i salta de
+    Serial.println("Resistencies en serie:");//Escriu el text i salta de
                            //línea.
-    Serial.println ();     //Deixa un espai en blanc.
+    Serial.println();      //Deixa un espai en blanc.
     
-    Serial.print ("RT = R1 + R2 = ");//Escriu el text.
-    Serial.print (r1);   //Escriu la variable r1.
-    Serial.print (" + ");   //Escriu +.
-    Serial.print (r2);   //Escriu la variable r2.
-    Serial.print(" = ");    //Escriu =.
+    Serial.print("RT = R1 + R2 = ");//Escriu el text.
+    Serial.print(r1);      //Escriu la variable r1.
+    Serial.print(" + ");   //Escriu +.
+    Serial.print(r2);      //Escriu la variable r2.
+    Serial.print(" = ");   //Escriu =.
     Serial.print (r1+r2);//Escriu el resultat de la suma de les variables.
-    Serial.println ("ohms");//Escriu el text i salta de línea.
-    Serial.println ();   //Deixa un espai en blanc.
+    Serial.println("ohms");//Escriu el text i salta de línea.
+    Serial.println();      //Deixa un espai en blanc.
+
+    delay(1000);           //Espera 1 segon.
     
     Serial.println ("Resistencies en paral.lel:");//Escriu el text i salta
                            //de línea.
     Serial.println ();     //Deixa un espai en blanc.
     
-    Serial.print("RT = 1 / (( 1 / R1 ) + ( 1 / R2)) = 1 / ( ");//Escriu el 
+    Serial.print("RT = 1/(( 1/R1 ) + ( 1/R2)) = 1/( ");//Escriu el 
                            //text.
-    Serial.println (1/r1); //Fa la divisió de 1 entre la variable r1.
-    Serial.print (" + ");  //Escriu +.
-    Serial.println (1/r2); //Fa la divisió de 1 entre la variable r2.
+    Serial.print(1/r1);    //Fa la divisió de 1 entre la variable r1.
+    Serial.print(" + ");   //Escriu +.
+    Serial.print(1/r2);    //Fa la divisió de 1 entre la variable r2.
     Serial.print(" ) = "); //Escriu ) =.
-    
+    Serial.print(" 1/"); //Escriu el text.
+    Serial.print(1/r1+1/r2);//Escriu el resultat de la operació.
+    Serial.print(" ) = "); //Escriu ) =.
+    Serial.print(1/(1/r1+1/r2));
+    Serial.println("ohms");//Escriu el text i salta de línea.
+
+    Serial.println ();     //Deixa un espai en blanc.
+    delay(3000);           //Espera 3 segons.
+    Serial.println("Entra nous valors per R1 i R2");//Escriu el text i s.lin.
+    Serial.println ();     //Deixa un espai en blanc.
     }
   }
 }
